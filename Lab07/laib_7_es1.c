14fc1025521ee9e769f8ae1f4cbb404f72acafb3
BCPTe <56164897+BCPTe@users.noreply.github.com>
2019-11-27 15:39:34 +0100

    Update laib_7_es1.c

--- Lab07/laib_7_es1.c
+++ Lab07/laib_7_es1.c
@@ -1,10 +1,10 @@
 /*
 =============================================================
-Name        : laib_X_esX.c
+Name        : laib_7_es1.c
 Author      : BCPTe
 Version     : 1.0
 Copyright   : ++NONE++
-Description : Laib_X Exercise X - APA 19/20 PoliTO
+Description : Laib_7 Exercise 1 - APA 19/20 PoliTO
 =============================================================
 */
 
