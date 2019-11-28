4533da78625b81ad42c5ef0148dc14cf2aba2725
BCPTe <56164897+BCPTe@users.noreply.github.com>
2019-11-28 22:09:01 +0100

    speeded up exec time

--- Lab07/laib_7_es1.c
+++ Lab07/laib_7_es1.c
@@ -8,33 +8,46 @@ Description : Laib_7 Exercise 1 - APA 19/20 PoliTO
 =============================================================
 */
 
+/*
+    PER VERIFICARE DIRETTAMENTE IL FILE DI TEST, COMMENTARE DA RIGA -37- A RIGA -41- E "SCOMMENTARE" LE PRTI ATTUALMENTE COMMENTATE.
+    IL NOME DEL FILE DI TESTO PER IL TEST VA CAMBIATO ALL'INTERNO DELLA fopen ALLA RIGA -42-.
+    FARE ATTENZIONE A "SCOMMENTARE" ANCHE LA PARENTESI A RIGA -64-.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define difstonesnumber 4
 int actualmax=1;
+int flag=0;
 
 typedef struct{
     int zaffiri, smeraldi, rubini, topazi;
     int tot;
 }stones;
 
-void createnecklace(int pos, char *val, char *sol, char *bestsol, stones total);
+void createnecklace(int pos, char *val, char *sol, char *bestsol, int k, stones total);
 
 int main(){
-    int i;
+    int i, measure;
     char value[difstonesnumber+1]="ZSRT";
     char *sol, *bestsol;
     stones numberstones;
 
     printf("Enter number of ZAFFIRI, SMERALDI, RUBINI, TOPAZI: ");
     scanf("%d%d%d%d", &numberstones.zaffiri, &numberstones.smeraldi, &numberstones.rubini, &numberstones.topazi);
-    numberstones.tot=numberstones.zaffiri+numberstones.smeraldi+numberstones.rubini+numberstones.topazi;
-
+    numberstones.tot=measure=numberstones.zaffiri+numberstones.smeraldi+numberstones.rubini+numberstones.topazi;
+/*
+    FILE *fp=fopen("INSERISCI_QUI_NOME_FILE", "r");
+    while(fscanf(fp, "%d%d%d%d", &numberstones.zaffiri, &numberstones.rubini, &numberstones.topazi, &numberstones.smeraldi)!=EOF){
+    numberstones.tot=measure=numberstones.zaffiri+numberstones.smeraldi+numberstones.rubini+numberstones.topazi;
+    flag=0; actualmax=1;
+*/
     sol=calloc(numberstones.tot+1, sizeof(char));
     bestsol=calloc(numberstones.tot+1, sizeof(char));
-    createnecklace(0, value, sol, bestsol, numberstones);
+    for( ; measure>0 && !flag; measure--)
+        createnecklace(0, value, sol, bestsol, measure, numberstones);
 
     printf("Total stones: %d!\n\n", numberstones.tot);
     printf("Best sol:\n");
@@ -46,41 +59,73 @@ int main(){
     printf("LENGTH: %d!\n", actualmax);
     printf("------------\n");
 
+    free(sol);
+    free(bestsol);
+//    }
     return EXIT_SUCCESS;
 }
 
-void createnecklace(int pos, char *val, char *sol, char *bestsol, stones total){
+void createnecklace(int pos, char *val, char *sol, char *bestsol, int k, stones total){
     int i;
 
-    if(pos>actualmax){          /* terminazione */
+    if(pos>=k){          /* terminazione */
+        flag=1;
         actualmax=pos;
         strcpy(bestsol, sol);
         return;
     }
 
-    for(i=0 ; i<strlen(val) ; i++){
+    for(i=0 ; i<strlen(val) && !flag ; i++){
         if(val[i]=='Z' && total.zaffiri>0){
             total.zaffiri--; total.tot--;           /* rimuovo le pietre gia' utilizzate */
             sol[pos]=val[i];
-            createnecklace(pos+1, "ZR", sol, bestsol, total);
+            if(total.zaffiri==0 && total.rubini>0)
+                createnecklace(pos+1, "R", sol, bestsol, k, total);
+            else if(total.rubini==0 && total.zaffiri>0)
+                createnecklace(pos+1, "Z", sol, bestsol, k, total);
+            else
+                createnecklace(pos+1, "ZR", sol, bestsol, k, total);
+            if(flag)
+                return;
             total.zaffiri++; total.tot++;           /* BACKTRACK */
         }
         if(val[i]=='S' && total.smeraldi>0){
             total.smeraldi--; total.tot--;          /* rimuovo le pietre gia' utilizzate */
             sol[pos]=val[i];
-            createnecklace(pos+1, "ST", sol, bestsol, total);
+            if(total.smeraldi==0 && total.topazi>0)
+                createnecklace(pos+1, "T", sol, bestsol, k, total);
+            else if(total.topazi==0 && total.smeraldi>0)
+                createnecklace(pos+1, "S", sol, bestsol, k, total);
+            else
+                createnecklace(pos+1, "ST", sol, bestsol, k, total);
+            if(flag)
+                return;
             total.smeraldi++; total.tot++;          /* BACKTRACK */
         }
         if(val[i]=='R' && total.rubini>0){
             total.rubini--; total.tot--;            /* rimuovo le pietre gia' utilizzate */
             sol[pos]=val[i];
-            createnecklace(pos+1, "ST", sol, bestsol, total);
+            if(total.smeraldi==0 && total.topazi>0)
+                createnecklace(pos+1, "T", sol, bestsol, k, total);
+            else if(total.topazi==0 && total.smeraldi>0)
+                createnecklace(pos+1, "S", sol, bestsol, k, total);
+            else
+                createnecklace(pos+1, "ST", sol, bestsol, k, total);
+            if(flag)
+                return;
             total.rubini++; total.tot++;            /* BACKTRACK */
         }
         if(val[i]=='T' && total.topazi>0){
             total.topazi--; total.tot--;            /* rimuovo le pietre gia' utilizzate */
             sol[pos]=val[i];
-            createnecklace(pos+1, "ZR", sol, bestsol, total);
+            if(total.zaffiri==0 && total.rubini>0)
+                createnecklace(pos+1, "R", sol, bestsol, k, total);
+            else if(total.rubini==0 && total.zaffiri>0)
+                createnecklace(pos+1, "Z", sol, bestsol, k, total);
+            else
+                createnecklace(pos+1, "ZR", sol, bestsol, k, total);
+            if(flag)
+                return;
             total.topazi++; total.tot++;            /* BACKTRACK */
         }
     }
